02bc6e0fcb98daaa052f8891fecacc1850e360b2
dark7void <yogurtyy@yahoo.com>
2023-01-22 23:47:29 +0100

    Update find-zeros-sha256.c

--- find-zeros-sha256.c
+++ find-zeros-sha256.c
@@ -2,37 +2,29 @@
 #include <string.h>
 #include <openssl/sha.h>
 #include <time.h>
-#include <stdlib.h>
 
 int main() {
-    srand(time(0));
+    int how_many_zeros = 6;
+    clock_t start_time = clock();
     int count = 0;
-    time_t start = time(NULL);
-    while (1) {
-        char data[20];
-        for (int i = 0; i < 20; i++) {
-            data[i] = rand() % 26 + 'a';
-        }
-        data[19] = '\0';
-
-        unsigned char hash[SHA256_DIGEST_LENGTH];
-        SHA256_CTX sha256;
-        SHA256_Init(&sha256);
-        SHA256_Update(&sha256, data, strlen(data));
-        SHA256_Final(hash, &sha256);
-
-        if (hash[0] == 0 && hash[1] == 0 && hash[2] == 0 && hash[3] == 0) {
+    unsigned long long num = 0;
+    char number[20];
+    sprintf(number, "dark7void%llu", num);
+    unsigned char hashed_number[SHA256_DIGEST_LENGTH];
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    while(1) {
+        sprintf(number + 9, "%llu", num);
+        num++;
+        SHA256_Update(&sha256, number, strlen(number));
+        SHA256_Final(hashed_number, &sha256);
+        SHA256_Init(&sha256);  // reset the context for the next hash
+        if (hashed_number[0] == 0 && hashed_number[1] == 0 && hashed_number[2] == 0 /*&& hashed_number[3] == 0 && hashed_number[4] < 16*/) {
+            clock_t end_time = clock();
             count++;
-            time_t end = time(NULL);
-            double rate = (double) count / difftime(end, start);
-            printf("Found another! rate: %f\n", rate);
-            printf("%s\n", data);
-        }
-        if (count == 100) {
-            break;
+            printf("found: %s\n", number);
+            printf("Hashes per second: %f\n", (double) count / ((double)(end_time - start_time) / CLOCKS_PER_SEC));
         }
     }
-    printf("Found %d hashes with 6 leading zeros!\n", count);
-    
     return 0;
 }
