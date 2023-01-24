180071196c140418e9e9c201a670bb4db49d02cd
dark7void <yogurtyy@yahoo.com>
2023-01-24 03:51:00 +0100

    Test (#1)
    * Update find-zeros-sha256.c
    
    * Update find-zeros-sha256.c

--- find-zeros-sha256.c
+++ find-zeros-sha256.c
@@ -4,14 +4,13 @@
 #include <time.h>
 
 int main() {
-	double last_rate;
-	double average_rate;
-	double last_50_rates[50];
+
+        double last_rate;
+        double last_50_rates[50];
     for (int i = 0; i < 49; i++) {
         last_50_rates[i] = 0;
     }
-    int how_many_zeros = 6;
-    clock_t start_time = clock();
+        int found_num;
     int count = 0;
     unsigned long long num = 0;
     char number[20];
@@ -19,6 +18,7 @@ int main() {
     unsigned char hashed_number[SHA256_DIGEST_LENGTH];
     SHA256_CTX sha256;
     SHA256_Init(&sha256);
+    clock_t start_time = clock();
     while(1) {
         sprintf(number + 10, "%llu", num);
         num++;
@@ -26,39 +26,41 @@ int main() {
         SHA256_Final(hashed_number, &sha256);
         SHA256_Init(&sha256);  // reset the context for the next hash
         if (hashed_number[0] == 0 && hashed_number[1] == 0 && hashed_number[2] == 0 /*&& hashed_number[3] == 0 && hashed_number[4] < 16*/) {
+                        printf("diff last number: %llu\n\n", num-found_num);
+                        found_num = num;
             clock_t end_time = clock();
             count++;
             printf("found: %s\n", number);
             double rate = (double) count / ((double)(end_time - start_time) / CLOCKS_PER_SEC);
-		    for (int i = 0; i < 49; i++) {
-		        last_50_rates[i] = last_50_rates[i + 1];
-		    }
-		    double non = 0;
-		    for (int i = 0; i < 49; i++) {
-		        if (last_50_rates[i] == 0) {
-		        	non++;
-		        }
-		    }
-		    last_50_rates[49] = rate;
+                    for (int i = 0; i < 49; i++) {
+                        last_50_rates[i] = last_50_rates[i + 1];
+                    }
+                    double non = 0;
+                    for (int i = 0; i < 49; i++) {
+                        if (last_50_rates[i] == 0) {
+                                non++;
+                        }
+                    }
+                    last_50_rates[49] = rate;
 
             printf("hashes per second: %.3f", rate);
 
-		    double sum = 0;
-		    for (int i = 0; i < 50; i++) {
-		        sum += last_50_rates[i];
-		    }
-		    double average = sum / (50-non);
+                    double sum = 0;
+                    for (int i = 0; i < 50; i++) {
+                        sum += last_50_rates[i];
+                    }
+                    double average = sum / (50-non);
             printf("  (last 50 average: %.3f)", average);
             double change = rate-last_rate;
             printf("  (change: %.3f", change);
             last_rate = (double) count / ((double)(end_time - start_time) / CLOCKS_PER_SEC);
 
             if (change < 0) {
-            	printf(" 🔻)\n");
+                printf(" 🔻)\n");
             } else if (change == 0) {
-            	printf(" --)\n");
+                printf(" --)\n");
             } else {
-            	printf(" ↑)\n");
+                printf(" ↑)\n");
             }
         }
     }
